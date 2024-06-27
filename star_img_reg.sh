c9df1a5cb32866ace57898113f580e020cbfd8ce
Samuel Oluwade <samueloluwade.dev@gmail.com>
2024-06-27 03:24:54 -0500

    Update star_img_reg.sh

--- star_img_reg.sh
+++ star_img_reg.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 533266979424.dkr.ecr.us-east-1.amazonaws.com
+aws ecr create-repository --repository-name star-facts-service
+docker build -t star-facts-service .
 docker tag star-facts-service:latest 533266979424.dkr.ecr.us-east-1.amazonaws.com/star-facts-service:latest
 docker push 533266979424.dkr.ecr.us-east-1.amazonaws.com/star-facts-service:latest
